8c5ebc80855b89acd52b0d5548597f40adfd0f9d
dkeppel <Sam-Trathen@live.nmit.ac.nz>
2024-09-12 11:54:18 +1200

    Pair-coding Project 1 JS

--- src/index.js
+++ src/index.js
@@ -1,19 +1,83 @@
-// Setting constants that will be used throughout the file
-// These constants all refer to elements within the DOM, retrieved by document.getElementById()
-const modal = document.getElementById('modal');
-const modalClose = document.getElementById('modal-close');
-const bookmarkForm = document.getElementById('bookmark-form');
-const websiteNameEl = document.getElementById('website-name');
-const websiteUrlEl = document.getElementById('website-url');
-const bookmarksContainer = document.getElementById('bookmarks-container');
+// Declaring constant variables, Retrieves elements from HTML by there ID so that they can be further manipulated by the JS
+const model = document.getElementById("model");
+const modelShow = document.getElementById("show-model");
+const modelClose = document.getElementById("model-close");
+const bookmarkForm = document.getElementById("bookmark-form");
+const websiteNameEl = document.getElementById("website-name");
+const websiteUrlEl = document.getElementById("website-url");
+const bookmarkContainer = document.getElementById("bookmark-container"); 
 
 let bookmarks = [];
 
-function showModal() {
-  modal.classList.add('show-modal');
-  websiteNameEl.focus();
-}
+function modelShow() {
+    model.classList.add("show-model")
+    websiteNameEl.focus()
+};
 
-modalClose.addEventListener('click', () => {
-  modal.classList.remove('show-modal');
+modelShow.addEventListener("click", showModel);
+
+modelClose.addEventListener("click", () => {
+    modelClose.classList.remove("show-model")
 });
+
+window.addEventListener("click", (e) => {
+    e.target === model ? model.classList.remove("show-model") : false
+});
+
+modelClose.addEventListener("click", () => {
+    model.classList.remove("show-model")
+});
+
+function validate(nameValue, urlValue) {
+    const expression =
+        /(https)?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=*^]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&//=]*)/g;
+        
+    const regex = new RegExp(expression)
+
+    if(!nameValue || !urlValue) {
+        alert("Please submit a values for both fields.")
+        return false
+    }
+    if(!urlValue.match(regex)) {
+        alert("Please provide valid a web address.")
+        return false
+    }
+    //If valid return true.
+    return true
+};
+
+function buildBookMarkDOM() {
+    bookmarkContainer.textContent= ""
+
+
+bookmarks.forEach((bookmarks) => {
+    const {name, url} = bookmark
+    //console.log(name, url)
+
+    const item = document.createElement("div")
+    item.classList.add("item")
+
+    const closeIcon = document.createElement("i")
+    closeIcon.classList.add("fas", "fa-times")
+    closeIcon.setAttribute("title", "Delete Bookmark")
+    closeIcon.setAttribute("onclick", `deleteBookmark("${url}")`)
+
+    const linkInfo = document.createElement("div")
+    linkInfo.classList.add("name")
+
+    const favicon = document.createElement("img")
+    favicon.setAttribute(
+        "src",url
+    )
+    favicon.setAttribute("alt", "favicon")
+
+    const link = document.createElement("a")
+    link.setAttribute("href", `${url}`)
+    link.setAttribute("target", "_blank")
+    link.textContent = name
+
+    linkInfo.append(favicon, link)
+    item.append(closeIcon, linkInfo)
+    bookmarkContainer.appendChild(item)
+})
+};
